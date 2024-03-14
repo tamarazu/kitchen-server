30755353774b8fe9c0af9efe54d765146f8200f4
tamarazu <tamarazulaika.u@gmail.com>
2024-03-14 21:11:06 +0700

    change dockrtfile

---
+++ server/config/config.js
@@ -0,0 +1,23 @@
+const env = process.env.NODE_ENV || "development";
+
+switch (env) {
+  case "development":
+    require("dotenv").config({
+      path: process.cwd() + "/.env",
+    });
+    break;
+  case "test":
+    require("dotenv").config({
+      path: process.cwd() + ".env.test",
+    });
+    break;
+}
+
+module.exports = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  dialect: "postgres",
+  operatorsAliases: 0,
+};
